57dfb23d14072b71b4d872a9d526d5044317f0bf
luisdev97 <amadormontorodavid@gmail.com>
2022-04-24 16:34:47 +0100

    refactor: remove hash logic in create user service

--- src/modules/user/src/application/create-user/create-user.service.ts
+++ src/modules/user/src/application/create-user/create-user.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from '../../domain/entities/user.entity';
-import { genSaltSync, hashSync } from 'bcrypt';
 import { CreateUserResponseDto } from './create-user.response.dto';
 
 @Injectable()
@@ -14,12 +13,9 @@ export class CreateUserService {
   ) {}
 
   async execute(params: CreateUserInputDTO): Promise<CreateUserResponseDto> {
-    const SALT_ROUNDS = 15;
-    const salt = genSaltSync(SALT_ROUNDS);
-
     const newUser = this.userRepository.create({
       email: params.email,
-      password: hashSync(params.password, salt),
+      password: params.password,
     });
 
     console.log(newUser);

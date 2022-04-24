1e533dd1f4702d664ae24a124583c3316da8c62b
luisdev97 <amadormontorodavid@gmail.com>
2022-04-24 16:35:55 +0100

    feat: login service to get token almost done

---
+++ src/modules/auth/src/application/login/login.service.ts
@@ -0,0 +1,44 @@
+import { LoginResponseDto } from './login.response.dto';
+import { UserEntity } from './../../../../user/src/domain/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { LoginInputDto } from '../../infrastructure/controllers/v1/login/login.input.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { compare as comparePasswords } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+export class LoginService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    private jwtService: JwtService,
+  ) {}
+
+  private async findUserForLogin({
+    email,
+    password,
+  }: LoginInputDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    const areEqual = await comparePasswords(password, user.password);
+    console.log(password);
+    console.log(user.password);
+    console.log(areEqual);
+
+    if (!areEqual) {
+      throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+    }
+
+    return user;
+  }
+
+  async execute(params: LoginInputDto): Promise<LoginResponseDto> {
+    const user = await this.findUserForLogin(params);
+    const jwt = await this.jwtService.sign({ id: user.id, email: user.email });
+    return { token: jwt };
+  }
+}
